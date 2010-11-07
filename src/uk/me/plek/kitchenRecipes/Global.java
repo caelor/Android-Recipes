19f82a0ab660d4bb9f4b2596b05acaaa1a448452
Andy Boff <andy.boff@gmail.com>
2010-11-07 00:53:23 +0000

    Bumped version number, added credits and variable for ACRA crash reporting
    (Basically getting ready for a bit more "in the wild" testing)

--- src/uk/me/plek/kitchenRecipes/Global.java
+++ src/uk/me/plek/kitchenRecipes/Global.java
@@ -2,4 +2,5 @@ package uk.me.plek.kitchenRecipes;
 
 public class Global {
 	static final String TAG = "RecipeBrowser";
+	static final String RELEASE = "0.2dev";
 }
--- src/uk/me/plek/kitchenRecipes/RecipeBrowserApplication.java
+++ src/uk/me/plek/kitchenRecipes/RecipeBrowserApplication.java
@@ -1,9 +1,16 @@
 package uk.me.plek.kitchenRecipes;
 
 import org.acra.CrashReportingApplication;
+import org.acra.ErrorReporter;
 
 public class RecipeBrowserApplication extends CrashReportingApplication {
 
+	public RecipeBrowserApplication() {
+		super();
+		
+		ErrorReporter.getInstance().addCustomData("Release", Global.RELEASE);
+	}
+	
 	@Override
 	public String getFormId() {
 		return "dE1LRHgxNktCaTg4ekpBUU5LQ2x1VEE6MQ";
